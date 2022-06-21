60de1de5e950e6cf4ead6d691b22ad86d0496c39
ahnaft7 <tajwara@bu.edu>
2022-06-21 01:27:02 -0400

    media

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-/*document.querySelector('#contact-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    e.target.elements.name.value = '';
-    e.target.elements.email.value = '';
-    e.target.elements.message.value = '';
-  });
-*/
-
 function validateForm() {
     let name = document.getElementById("name");
     let letters = /^[A-Za-z]+$/;
